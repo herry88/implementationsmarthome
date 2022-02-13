9bac45bdff2a1dabfbd6d75894fac09179d53572
Herry Prasetyo <herry081288@gmail.com>
2022-02-13 16:04:50 +0700

    smarthome

---
+++ app/Http/Controllers/LightController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Light;
+use Illuminate\Http\Request;
+
+class LightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Light  $light
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Light $light)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Light  $light
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Light $light)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Light  $light
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Light $light)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Light  $light
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Light $light)
+    {
+        //
+    }
+}
